65b06a8024620f026c15d2a12e1443a824551f76
MD SABIR ANSARI <85472587+MdShabir06@users.noreply.github.com>
2021-06-07 12:52:45 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Note() {
+  return (
+          <div>
+              <div className="note">
+                    <h1>Javascript and React.js</h1>
+                    <p>
+                    I had joined an awesome Webdevelopment with Javascript and ReactJS bootcamp taken by Sourya Sinha,in which we learn html, css, JavaScript, and ReactJS.
+                    </p>
+              </div>
+              <div className="note">
+                    <h1>Python Machine Leaning</h1>
+                    <p>
+                          I had joined an amazing machiny learning bootcamp taken by Sourya Sinha,in which we learn use full libraries in Machine Leaning like Numpy Pandas Matplotlib etc.
+                    </p>
+              </div>
+              <div className="note">
+                    <h1>Cyber Sequrity</h1>
+                    <p>
+                          Shape AI also conduct a bootcamp on a Cyber Security in which it teaches basics of Cyber Crime and How to prevent?
+                    </p>
+              </div>
+              <div className="note">
+                    <h1>Front End developer</h1>
+                    <p>
+                        I had joined an awesome front end development with html, css, and javascript bootcamp taken by Sourya Sinha,in which we learn html, css, and JavaScript.
+                    </p>
+              </div>
+          </div>
+  );
+}
+
+export default Note;
